5644aeb043206c5e632126fbe3c0a5036e7f9b72
oronibrian <brianoroni6@gmail.com>
2018-07-04 13:53:02 +0300

    RST instruction

---
+++ oronicomments.rst
@@ -0,0 +1,13 @@
+**Oroni Brians Comments and Instructions**
+
+*Managing static*
+
+what You did.
+  You did not include static root
+
+
+how to
+  Define a static root folder in the settings file
+
+  1. STATIC_ROOT='tech/static'
+
--- simple/settings.py
+++ simple/settings.py
@@ -124,6 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT='tech/static'
+
+
+
 STATICFILES = [BASE_DIR + "/static/"
 ]
